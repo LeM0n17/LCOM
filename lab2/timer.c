6e4ca4856d0ebd53cdf4af2e6857082b003d28db
Shamiko <gpnpinho@gmail.com>
2023-02-23 16:58:37 +0000

    Work in progress

---
+++ lab2/timer.c
@@ -0,0 +1,70 @@
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
+
+#include <stdint.h>
+
+#include "i8254.h"
+
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (timer_subscribe_int)(uint8_t *bit_no) {
+    /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (timer_unsubscribe_int)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+void (timer_int_handler)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+}
+
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+  if(timer > 2){
+    return 1;
+  }
+  uint32_t rb = (uint32_t)TIMER_RB_CMD|TIMER_RB_COUNT_|TIMER_RB_SEL(timer);
+  int out_flag = sys_outb(TIMER_CTRL, rb);
+  if(out_flag == 1){
+    return 1;
+  }
+  int in_flag = util_sys_inb(TIMER_CTRL, st);
+  return in_flag;
+
+}
+
+int (timer_display_conf)(uint8_t timer, uint8_t st,
+                        enum timer_status_field field) {
+  union timer_status_field_val val;
+  switch(field){
+    case tsf_all:
+      val.byte = st;
+      break;
+    case tsf_initial:
+      break;
+    case tsf_mode:
+      break;
+    case tsf_base:
+      break;
+    default:
+      return 1;
+
+  }
+  timer_print_config(timer, field,val);
+
+
+
+  return 1;
+}
