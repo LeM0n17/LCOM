947ddc5d1a3f16a2e809a41ec681fa06766b72c2
shamiko <up202108672@up.pt>
2023-04-25 20:24:15 +0100

    Added collision with arena walls

--- proj/src/object.c
+++ proj/src/object.c
@@ -3,19 +3,35 @@
 #include "../headers/object.h"
 
 void moveUp(Object* obj, uint16_t speed){
-    obj->y -= speed;
+    if(obj->y - speed <= 50){
+        obj->y -= (obj->y - 50);
+    } else {
+        obj->y -= speed;
+    }
 }
 
 void moveDown(Object* obj, uint16_t speed){
-    obj->y += speed;
+    if(obj->y + 50 + speed >= 974){
+        obj->y += (974 - obj->y - 50);
+    } else {
+        obj->y += speed;
+    }
 }
 
 void moveLeft(Object* obj, uint16_t speed){
-    obj->x -= speed;
+    if(obj->x - speed <= 50){
+        obj->x -= (obj->x - 50);
+    }else{
+        obj->x -= speed;
+    }
 }
 
 void moveRight(Object* obj, uint16_t speed){
-    obj->x += speed;
+    if(obj->x + 50 + speed >= 1230){
+        obj->x += (1230 - obj->x - 50);
+    } else {
+        obj->x += speed;
+    }
 }
 
 void process_scancode(Object* obj, uint8_t scan_codes[]){
--- proj/src/video.c
+++ proj/src/video.c
@@ -166,7 +166,7 @@ int (vg_update)(xpm_map_t xpm, uint16_t old_x, uint16_t old_y, uint16_t new_x, u
 
 int (vg_update2)(Object obj, uint16_t old_x, uint16_t old_y){
     if(obj.x != old_x || obj.y != old_y){
-        if(vg_draw_rectangle(old_x, old_y, 100, 100 ,0XFFF0)){
+        if(vg_draw_rectangle(old_x, old_y, 50, 50 ,0XFFF0)){
             return 1;
         }
         if(vg_draw_rectangle(obj.x, obj.y, 50, 50 ,0X000F)){
