29cf088a78c95d833dbbdd48fed87e5f26f0d557
Sucena <tomas21sucena@gmail.com>
2023-05-29 01:01:18 +0100

    documented KBC.h and its devices

--- proj/src/devices/KBC/keyboard.h
+++ proj/src/devices/KBC/keyboard.h
@@ -15,6 +15,12 @@
 #define breakcode(makecode) (makecode | BIT(7))
 #define is_makecode(scancode) !(scancode & BIT(7))
 
+/**
+ * @brief struct representing the keyboard data the KBC provides
+ * @param two_byte indicates if the current scancode is part of a two-byte scancode or not
+ * @param valid indicates if the data is valid
+ * @param error indicates if there was an error retrieving the data
+ */
 typedef struct {
     uint8_t scancodes[2];
     bool two_byte;
@@ -23,10 +29,31 @@ typedef struct {
 } kbd_data_t;
 
 // for interrupts
+/**
+ * @brief attempts to enables keyboard interrupts
+ * @param wait_ticks the number of attempts that will be made
+ * @return zero on success, non-zero otherwise
+ */
 int (kbd_enable_int)(uint32_t wait_ticks);
+
+/**
+ * @brief subscribes the keyboard interrupts
+ * @param bit_no pointer to a number representing the position dedicated to the keyboard in the interrupt vector
+ * @return zero on success, non-zero otherwise
+ */
 int (kbd_subscribe_int)(uint8_t *bit_no);
+
+/**
+ * @brief unsubscribes keyboard interrupts
+ * @return zero on success, non-zero otherwise
+ */
 int (kbd_unsubscribe_int)();
 
+/**
+ * @brief attempts to read a scancode from the KBC
+ * @param kbd_data the struct where the scancode will be placed
+ * @param wait_ticks the number of attempts that will be made
+ */
 void (kbd_get_scancode)(kbd_data_t* kbd_data, uint32_t wait_ticks);
 
 #endif // _LCOM_KEYBOARD_H_
